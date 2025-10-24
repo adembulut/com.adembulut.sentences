e0fbb54be3fa7874e133c190b19cf2c70c8a28f8
adem.bulut <adembulutapp@gmail.com>
2025-10-24 21:27:15 +0300

    Initial Commit

---
+++ sentencesTests/sentencesTests.swift
@@ -0,0 +1,17 @@
+//
+//  sentencesTests.swift
+//  sentencesTests
+//
+//  Created by adem bulut on 24.10.2025.
+//
+
+import Testing
+@testable import sentences
+
+struct sentencesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
